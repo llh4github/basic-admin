71fcadc2bd762ac43d5bdd4e71da9f1038768784
lilinhong <forever2y_y@163.com>
2020-10-27 15:56:50 +0800

    init project

---
+++ basic-admin-server/src/main/kotlin/com/llh/basicadmin/BasicAdminApplication.kt
@@ -0,0 +1,11 @@
+package com.llh.basicadmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BasicAdminApplication
+
+fun main(args: Array<String>) {
+    runApplication<BasicAdminApplication>(*args)
+}
---
+++ basic-admin-server/src/test/kotlin/com/llh/basicadmin/BasicAdminApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.llh.basicadmin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BasicAdminApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
