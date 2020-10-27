946a4cfae42e6062afd769de4cca34a1a30821e6
llh <leelinhong@outlook.com>
2020-10-27 21:36:10 +0800

    修改用户表

--- basic-admin-server/src/main/resources/db/migration/V1.0.1__Add_table_user.sql
+++ basic-admin-server/src/main/resources/db/migration/V1.0.1__Add_table_user.sql
@@ -1,11 +1,17 @@
 use `basic_admin`;
 CREATE TABLE `sys_user`
 (
-    id            int auto_increment,
-    `username`    varchar(100) NULL,
-    `password`    varchar(100) NULL,
-    `create_time` datetime     NULL ON UPDATE CURRENT_TIMESTAMP,
-    `update_time` datetime     NULL ON UPDATE CURRENT_TIMESTAMP,
+    id             int auto_increment,
+
+    `status`       tinyint(1)   DEFAULT NULL,
+    `password`     varchar(255) DEFAULT NULL,
+    `username`     varchar(30)  DEFAULT NULL,
+
+    `created_time` datetime     DEFAULT NULL,
+    `created_by`   int          DEFAULT NULL,
+    `remove_flag`  tinyint(1)   DEFAULT 1,
+    `updated_time` datetime     DEFAULT NULL,
+    `updated_by`   int          DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) COMMENT ='系统用户表' ENGINE = InnoDB
                    DEFAULT CHARSET = utf8mb4;
--- basic-admin-server/src/main/resources/db/migration/V1.0.2__Modify_table_user.sql
+++
@@ -1,3 +0,0 @@
-use `basic_admin`;
-ALTER TABLE `sys_user`
-    ADD COLUMN `status` tinyint(3) NULL DEFAULT NULL COMMENT '用户状态' AFTER `update_time`;
