c5067a6284ccaac66039d928f4568a0a1d932f41
lilinhong <forever2y_y@163.com>
2020-10-27 18:24:52 +0800

    主键自增

--- basic-admin-server/src/main/resources/db/migration/V1.0.1__Add_table_user.sql
+++ basic-admin-server/src/main/resources/db/migration/V1.0.1__Add_table_user.sql
@@ -1,7 +1,7 @@
 use `basic_admin`;
 CREATE TABLE `sys_user`
 (
-    `id`          int          NOT NULL,
+    id            int auto_increment,
     `username`    varchar(100) NULL,
     `password`    varchar(100) NULL,
     `create_time` datetime     NULL ON UPDATE CURRENT_TIMESTAMP,
@@ -9,4 +9,3 @@ CREATE TABLE `sys_user`
     PRIMARY KEY (`id`)
 ) COMMENT ='系统用户表' ENGINE = InnoDB
                    DEFAULT CHARSET = utf8mb4;
-
