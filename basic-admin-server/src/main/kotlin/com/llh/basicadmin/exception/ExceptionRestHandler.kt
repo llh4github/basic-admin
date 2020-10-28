1bb150a128578dcc42431a55aca178fba26d3172
lilinhong <forever2y_y@163.com>
2020-10-28 18:09:07 +0800

    异常处理

---
+++ basic-admin-server/src/main/kotlin/com/llh/basicadmin/exception/ExceptionRestHandler.kt
@@ -0,0 +1,29 @@
+package com.llh.basicadmin.exception
+
+import com.llh.basicadmin.pojo.RespWrapper
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+/**
+ * 统一异常处理
+ * Created At 2020/10/28 17:20
+ *
+ * @author llh
+ */
+@RestControllerAdvice
+class ExceptionRestHandler {
+
+    /**
+     * 处理字段验证异常
+     */
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): RespWrapper {
+        val msg = e
+            .bindingResult
+            .fieldErrors
+            .joinToString(separator = ",")
+            { "" + it.defaultMessage }
+        return RespWrapper(5001, msg, null)
+    }
+}
