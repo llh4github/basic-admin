d4c71dd10c754e16fc6222fd439937b8e45b41f9
lilinhong <forever2y_y@163.com>
2020-10-29 17:32:06 +0800

    添加 validation约束

--- basic-admin-server/src/main/kotlin/com/llh/basicadmin/pojo/vo/RoleVO.kt
+++ basic-admin-server/src/main/kotlin/com/llh/basicadmin/pojo/vo/RoleVO.kt
@@ -3,9 +3,12 @@ package com.llh.basicadmin.pojo.vo
 import com.llh.basicadmin.common.validation.AddOperate
 import com.llh.basicadmin.common.validation.UpdateOperate
 import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+import org.hibernate.validator.constraints.Length
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
 
 /**
  *
@@ -15,11 +18,24 @@ import javax.validation.constraints.NotNull
  */
 @ApiModel("角色信息")
 data class RoleVO(
+
     @field:NotNull(message = "id不能为空", groups = [UpdateOperate::class])
     @field:Min(value = 0, message = "id值必须为正整数", groups = [UpdateOperate::class])
     val id: Int?,
+
+
     @field:NotBlank(message = "角色代号不能为空", groups = [AddOperate::class])
+    @field:Pattern(regexp = "^[A-Za-z0-9_]+\$",
+        message = "角色代号只能由英文字母、数字和下划线组成",
+        groups = [AddOperate::class, UpdateOperate::class])
+    @field:Length(max = 30, message = "角色代号在30个字符之内",
+        groups = [AddOperate::class, UpdateOperate::class])
+    @ApiModelProperty(value = "角色代号", notes = "角色代号只能由英文字母、数字和下划线组成")
     val roleName: String?,
+
+
     @field:NotBlank(message = "角色名不能为空", groups = [AddOperate::class])
-    val displayName: String?
+    @field:Length(max = 30, message = "角色名在30个字符之内",
+        groups = [AddOperate::class, UpdateOperate::class])
+    val displayName: String?,
 )
