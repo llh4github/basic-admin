bcdf18bc85e3154296de2307b446466447572197
lilinhong <forever2y_y@163.com>
2020-11-11 10:34:15 +0800

    json工具类配置

--- basic-admin-server/src/main/kotlin/com/llh/basicadmin/common/util/SpringUtils.kt
+++ basic-admin-server/src/main/kotlin/com/llh/basicadmin/common/util/SpringUtils.kt
@@ -1,9 +1,15 @@
 package com.llh.basicadmin.common.util
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import org.ktorm.jackson.KtormModule
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
 import org.springframework.core.env.Profiles
 import org.springframework.stereotype.Component
+
+val JacksonUtils = ObjectMapper()
 
 /**
  * Spring相关的工具类
@@ -47,5 +53,17 @@ object SpringUtils {
 class InitUtil : ApplicationContextAware {
     override fun setApplicationContext(applicationContext: ApplicationContext) {
         SpringUtils.applicationContext = applicationContext
+        initUtils()
+    }
+
+    /**
+     * 初始化工具类。
+     *
+     * 避免执行顺序问题就放到Spring组件里执行了
+     */
+    fun initUtils() {
+        JacksonUtils.registerModule(KtormModule())
+        JacksonUtils.registerModule(JavaTimeModule())
+        JacksonUtils.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     }
 }
